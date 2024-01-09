8c1001648e3e810dd3fe99441a61d9df5bc8a8d0
Ivan Stankovic <stankovicc.ivan@gmail.com>
2024-01-09 16:57:23 +0100

    add useEffect; add interface and dummy notes;

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import axios from 'axios'
+// import axios from 'axios'
+import DUMMY_NOTES from './DUMMY_NOTES'
 
 function App() {
     // use state to rerender UI
     const [notesList, setNotesList] = useState<Array<any>>([])
 
-    const getNotes = async () => {
-        try {
-            const response = await axios.get('http://localhost:5000/notes')
-            setNotesList(response.data.notes) // updates the "notesList"
-            console.log(notesList)
-        } catch (err) {
-            console.error(err)
-        }
-    }
+    useEffect(() => {
+        setNotesList(DUMMY_NOTES) // called the 1st time the component renders
+    }, []) // [] - not dependent on anything (only runs the 1st time)
+
+    // const getNotes = async () => {
+    //     try {
+    //         const response = await axios.get('http://localhost:5000/notes')
+    //         setNotesList(response.data.notes) // updates the "notesList"
+    //         console.log(notesList)
+    //     } catch (err) {
+    //         console.error(err)
+    //     }
+    // }
+
+    console.log(notesList);
+    
 
     return (
         <div className="App">
             <div>Notes App</div>
             <div>
-                <button onClick={getNotes}>Click me!</button>
+                {/* <button onClick={getNotes}>Click me!</button> */}
             </div>
             <div>
                 {/* show details of the first element */}
---
+++ src/DUMMY_NOTES.ts
@@ -0,0 +1,11 @@
+import INote from './interfaces/note.interface'
+
+const DUMMY_NOTES: INote[] = [
+    { text: 'Note 1', link: 'https://www.google.com' },
+    { text: 'Note 1', link: 'https://www.google.com' },
+    { text: 'Note 1', link: 'https://www.google.com' },
+    { text: 'Note 1', link: 'https://www.google.com' },
+    { text: 'Note 1', link: 'https://www.google.com' },
+]
+
+export default DUMMY_NOTES
