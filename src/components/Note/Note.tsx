ddd4b2be1ba2b306ffcbd9b00a6ac6dcd15de310
Ivan Stankovic <stankovicc.ivan@gmail.com>
2024-01-11 17:40:44 +0100

    add update on blur

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, FocusEvent } from 'react'
 import INote from '../../interfaces/note.interface'
 import './Note.css'
 
@@ -6,12 +6,25 @@ type Props = {
     note: INote
 }
 
+const noteTextUpdated = (event: FocusEvent<HTMLDivElement>) => {
+    console.log('Note text changed')
+    let textContent = event.currentTarget.textContent
+    console.log(textContent)
+}
+
 // FC - functional component
 const Note: FC<Props> = ({ note }) => {
     return (
         <div className="note">
             {/* show details of the first element */}
-            <div className="note__text">{note.text}</div>
+            <div
+                onBlur={noteTextUpdated}
+                contentEditable={true}
+                suppressContentEditableWarning={true}
+                className="note__text"
+            >
+                {note.text}
+            </div>
             <div className="note__link">
                 <a href={note.link}>{note.link}</a>
             </div>
